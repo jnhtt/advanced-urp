fa9501bf09dae5b64c42150729b730e1682b6b93
jnhtt <man.in.cyber.space@gmail.com>
2020-03-28 16:29:22 +0900

    add Chapter1

---
+++ Assets/Chapter1/Scripts/CustomRenderPass/ObjectRenderPassFeature.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Chapter1
+{
+    public class ObjectRenderPassFeature : ScriptableRendererFeature
+    {
+        private ObjectRenderPass currentPass;
+
+        public override void Create()
+        {
+            if (currentPass == null)
+            {
+                currentPass = new ObjectRenderPass();
+            }
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            currentPass.SetRenderTarget(renderer.cameraColorTarget);
+            renderer.EnqueuePass(currentPass);
+        }
+    }
+}
